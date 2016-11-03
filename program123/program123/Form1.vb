9dad9f1e37067ec7981d0c4ced1f6da4681f5538
Tamashihare <dila.felmi.@gmail.com>
2016-11-03 17:31:40 +0700

    Add files via upload

---
+++ program123/program123/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "jaka" And TextBox2.Text = "123" Then
+            Form2.Show()
+            MsgBox("Login Sucess", MsgBoxStyle.OkOnly, "Invalid")
+
+        Else
+            MsgBox("sorry ,Username or password  not found", MsgBoxStyle.OkOnly, "Invalid")
+        End If
+    End Sub
+End Class
